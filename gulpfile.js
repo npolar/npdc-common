a2f1d897b8d50204655dd1209418a690b27c6364
Anders Bälter <anders.balter@npolar.no>
2015-07-29 17:02:09 +0200

    angular material on the way

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,27 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
-var watch = require('gulp-watch');
 var del = require('del');
+var runSequence = require('run-sequence');
 var reload = browserSync.reload;
-var DEST = './dist';
+
+var config = {
+  src: {
+    root: 'src',
+    js: ['node_modules/angular-material/angular-material.js', './src/**/*.js', '!./src/**/*Spec.js'],
+    sass: ['./src/layout.scss','./src/components/**/*.scss']
+  },
+  dest: {
+    root: 'dist'
+  }
+};
 
 var handleError = function (err) {
   console.log(err.toString());
@@ -21,64 +30,72 @@ var handleError = function (err) {
 };
 
 // JavaScript tasks
-gulp.task('compile-js', ['clean-js'], function () {
-  return gulp.src(['./src/js/**/*.js'])
+gulp.task('compile-js', ['test-js'], function () {
+  return gulp.src(config.src.js)
+    .pipe(sourcemaps.init())
     .pipe(concat('npolar-css.js'))
-    .pipe(gulp.dest(DEST + '/js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('compile-js-min', function () {
+  return gulp.src(config.src.js)
+    .pipe(sourcemaps.init())
+    .pipe(concat('npolar-css.min.js'))
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest(DEST + '/js'));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.dest.root));
 });
 
-gulp.task('validate-js', [], function () {
-  return gulp.src(['./src/js/**/*.js'])
-    .pipe(jshint({ multistr: true }))
-    .pipe(jshint.reporter('default'));
-});
-
-gulp.task('test-js', [], function () {
-  return gulp.src(['./test/js/**/*.js'])
+gulp.task('test-js', ['validate-js'], function () {
+  return gulp.src(['./src/**/*Spec.js'])
     .pipe(mocha({ reporter: 'spec' }));
 });
 
+gulp.task('validate-js', function () {
+  return gulp.src(['./src/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Sass tasks
+gulp.task('compile-sass', function () {
+  return gulp.src(config.src.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', handleError))
+    .pipe(concat('npolar-css.css'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('compile-sass-min', function () {
+  return gulp.src(config.src.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', handleError))
+    .pipe(concat('npolar-css.min.css'))
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('clean', function (cb) {
+  del([config.dest.root], cb);
+});
+
+gulp.task('compile', ['clean'], function (cb) {
+  runSequence(['compile-sass', 'compile-js'], ['compile-sass-min', 'compile-js-min'], cb);
+});
+
 // Reload
-gulp.task('live-server', ['compile-less'], function () {
+gulp.task('live-server', ['compile'], function () {
   browserSync({ server: { baseDir: '.' }});
 });
 
 // Watch
-gulp.task('watch', ['live-server', 'copyfonts'], function () {
-  watch(['*.html', DEST + '/css/**/*.css', 'src/js/**/*.js'], reload);
-  watch(['./src/less/**/*.less'], function () {
-    gulp.start('compile-less');
-  });
-
+gulp.task('watch', ['live-server'], function () {
+  gulp.watch(['*.html', config.dest.root + '/**/*.css', config.dest.root + '/**/*.js'], reload);
+  gulp.watch(['./src/**/*.scss'], ['compile-sass', 'compile-sass-min']);
+  gulp.watch(['./src/**/*.js'], ['compile-js', 'compile-js-min']);
 });
 
-// Less tasks
-gulp.task('compile-less', ['clean-css'], function () {
-  return gulp.src('./src/less/main.less')
-    .pipe(sourcemaps.init())
-    .pipe(less().on('error', handleError))
-    .pipe(minifyCSS())
-    .pipe(sourcemaps.write('../maps'))
-    .pipe(gulp.dest(DEST + '/css'));
-});
-
-gulp.task('clean-js', [], function (cb) {
-  del([DEST + '/js'], cb);
-});
-
-gulp.task('clean-css', [], function (cb) {
-  del([DEST + '/css'], cb);
-});
-
-gulp.task('copyfonts', function() {
-   return gulp.src('./node_modules/bootstrap/dist/fonts/**/*')
-   .pipe(gulp.dest(DEST + '/fonts'));
-});
-
-gulp.task('default', [
-  'validate-js', 'test-js',
-  'compile-js', 'compile-less',
-  'live-server', 'watch']);
+gulp.task('default', ['compile', 'watch']);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+require('angular');
+require('angular-aria');
+require('angular-animate');
+require('angular-material');
+
+module.exports = {};
