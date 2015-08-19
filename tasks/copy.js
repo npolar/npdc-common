d3a7cd7d10d80ab4e03f277cfc04be96ae9ffa5b
Anders Bälter <anders.balter@npolar.no>
2015-08-19 17:56:40 +0200

    Copy demo stuffs

--- tasks/copy.js
+++ tasks/copy.js
@@ -3,19 +3,25 @@ var task = function(gulp, config) {
 
   var changed = require('gulp-changed');
 
-  gulp.task('copy-html', function () {
+  gulp.task('copy-html', function() {
     return gulp.src(config.src.html)
       .pipe(changed(config.dist.root))
       .pipe(gulp.dest(config.dist.root));
   });
 
-  gulp.task('copy-static', function () {
+  gulp.task('copy-static', function() {
     return gulp.src(config.src.img)
       .pipe(changed(config.dist.root))
       .pipe(gulp.dest(config.dist.root));
   });
 
-  gulp.task('copy-all', ['copy-html', 'copy-static']);
+  gulp.task('copy-demo', function() {
+    return gulp.src([].concat(config.src.demo, config.deps.demo))
+      .pipe(changed(config.dist.root))
+      .pipe(gulp.dest(config.dist.root));
+  });
+
+  gulp.task('copy-all', ['copy-html', 'copy-static', 'copy-demo']);
 };
 
 module.exports = task;
