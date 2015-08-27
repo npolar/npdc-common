5e4837d5d62190c025cf64158d2ee1615048870b
cnrdh <cnrdh@users.noreply.github.com>
2015-08-27 16:31:21 +0200

    Added rudimentary search to top bar

--- src/ui/Top/TopController.js
+++ src/ui/Top/TopController.js
@@ -1,9 +1,11 @@
 'use strict';
 
-// @ngInject
-var TopController = function ($scope, NpolarApiSecurity, NpdcBreadcrumbs) {
+// @ngInject  
+var TopController = function ($scope, $location, NpolarApiSecurity, NpdcBreadcrumbs) {
   $scope.security = NpolarApiSecurity;
   $scope.breadcrumbs = NpdcBreadcrumbs;
+  $scope.q = $location.search().q;
+  
 };
 
 module.exports = TopController;
