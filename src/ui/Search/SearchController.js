353ccd4faa8f70af02a691cccf71dd5ce4cfc75a
cnrdh <cnrdh@users.noreply.github.com>
2015-08-27 16:30:19 +0200

    Added chips for search filters

--- src/ui/Search/SearchController.js
+++ src/ui/Search/SearchController.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // @ngInject
-var SearchController = function ($scope, NpolarApiSecurity) {
+var SearchController = function ($scope, $location, NpolarApiSecurity) {
   
   $scope.security = NpolarApiSecurity;
-  
+  $scope.q = $location.search().q;
 };
 
 module.exports = SearchController;
