c7bcaa813ebde5712e28317aa1fad4af32e9dafe
cnrdh <cnrdh@users.noreply.github.com>
2015-09-01 11:17:26 +0200

    Rudimentary search with facets

--- src/ui/Search/SearchController.js
+++ src/ui/Search/SearchController.js
@@ -5,6 +5,23 @@ var SearchController = function ($scope, $location, NpolarApiSecurity) {
   
   $scope.security = NpolarApiSecurity;
   $scope.q = $location.search().q;
+  
+  // Simplify facet array to facet object
+  $scope.facet = function(farr) {
+    let keys = Object.keys(farr);
+    let key = keys[0];
+    let facet = farr[key].map(f => {
+      
+      f.facet = key;
+      return f;
+    
+    });
+    
+    return facet;
+    
+  };
+  
+  
 };
 
 module.exports = SearchController;
