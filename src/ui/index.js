e90f93bf7735fb1107ba94ad2f973163e6c0a562
cnrdh <cnrdh@users.noreply.github.com>
2015-09-01 15:53:48 +0200

    Removed comment

--- src/ui/index.js
+++ src/ui/index.js
@@ -18,8 +18,5 @@ ui.directive('npdcEditButton', require('./button/EditButton'));
 ui.directive('npdcSaveButton', require('./button/SaveButton'));
 ui.directive('npdcDeleteButton', require('./button/DeleteButton'));
 ui.controller('NpdcButtonComponent', require('./button/ButtonComponent'));
-//directive button new
-//<a ng-if="security.isAuthorized('create', resource.path)" href="__new/edit"><button type="button">New</button></a>
-
 
 module.exports = ui;
