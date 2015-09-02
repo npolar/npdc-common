a3f60b0f9ede71f1b2af9b08fa28314beb208cab
cnrdh <cnrdh@users.noreply.github.com>
2015-09-02 17:05:49 +0200

    Adding visible warning when running against production API outside data.npolar.no

--- src/ui/Top/TopController.js
+++ src/ui/Top/TopController.js
@@ -1,10 +1,17 @@
 'use strict';
 
 // @ngInject  
-var TopController = function ($scope, $location, NpolarApiSecurity, NpdcBreadcrumbs) {
+var TopController = function ($scope, $location, NpolarApiSecurity, NpdcBreadcrumbs, npolarApiConfig) {
   $scope.security = NpolarApiSecurity;
   $scope.breadcrumbs = NpdcBreadcrumbs;
   $scope.q = $location.search().q;
+  
+  if ("production" === npolarApiConfig.environment) {
+    if ("data.npolar.no" !== window.location.hostname) {
+      $scope.warning = `Running against production API ${ npolarApiConfig.base } from ${ window.location.href }`;
+    }
+  }
+  
 };
 
 module.exports = TopController;
