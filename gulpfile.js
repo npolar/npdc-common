964ca4841e50dd365a4f4d07344e2eed99ccd1d2
Anders Bälter <anders.balter@npolar.no>
2015-09-02 15:31:50 +0200

    Made npdc-common deployable

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@
 
 var gulp = require('gulp');
 var npdcGulp = require('npdc-gulp');
-
+var config = npdcGulp.baseConfig;
 npdcGulp.loadModuleTasks(gulp);
 
-gulp.task('copy-deps-assets', function () {
-  return gulp.src(['node_modules/npdc-material/dist/img/*'], { base: 'node_modules' })
+gulp.task('copy-deps-assets', ['clean'], function () {
+  return gulp.src(['node_modules/npdc-material/dist/img/*', 'node_modules/npdc-material/dist/*.css'], { base: 'node_modules/npdc-material/dist' })
     .pipe(gulp.dest('dist/assets'));
 });
 
-gulp.task('default', ['lint', 'test', 'copy-deps-assets']);
+gulp.task('default', ['copy-deps-assets', 'lint', 'test']);
+gulp.watch([].concat(config.src.js, config.tests), ['lint', 'test']);
