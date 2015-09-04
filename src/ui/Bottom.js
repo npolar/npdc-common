4ee74e4da2c3d04dadaa3baecda87c55708ebb01
cnrdh <cnrdh@users.noreply.github.com>
2015-09-04 10:56:19 +0200

    Added <npdc:bottom>

---
+++ src/ui/Bottom.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngInject
+ */
+var Bottom = function () {
+  return {
+    controller: 'NpdcBottomController',
+    templateUrl: 'npdc-common/src/ui/bottom/npdc-bottom.html'
+  };
+};
+
+module.exports = Bottom;
