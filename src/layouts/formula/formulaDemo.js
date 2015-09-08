dbe32bab7c7b0e498352493e2befa94b1a139726
Anders Bälter <anders.balter@npolar.no>
2015-09-08 13:52:00 +0200

    Removed testcode

--- src/layouts/formula/formulaDemo.js
+++ src/layouts/formula/formulaDemo.js
@@ -7,22 +7,9 @@ let angular = require('angular');
 angular
   .module('formulaDemo', ['npdcMaterial', 'formula'])
   .controller('FormulaCtrl', ($mdDialog, $scope, $compile) => {
-    $scope.show = false;
-    $scope.toggleShow = function () {
-      console.log("toggle", $scope.show);
-      $scope.show = !$scope.show;
-    };
     $scope.formulaData = {
       schema: "./demo/schema.json",
       form: "./demo/form.json",
       template: "material"
     };
-})
-  .directive("textarea", function() {
-  return {
-    restrict: "E",
-    controller: function($element) {
-      $element.attr("rows", 15);
-    }
-  };
 });
