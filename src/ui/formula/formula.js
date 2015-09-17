65143b526579d5835df576449f6c1ee486544bf2
Anders Bälter <anders.balter@npolar.no>
2015-09-17 11:32:28 +0200

    Removed debug log

--- src/ui/formula/formula.js
+++ src/ui/formula/formula.js
@@ -5,10 +5,7 @@
  */
 var formula = function () {
   return {
-    templateUrl: 'npdc-common/src/ui/formula/edit.html',
-    link (scope) {
-      console.log('formula', scope.formula);
-    }
+    templateUrl: 'npdc-common/src/ui/formula/edit.html'
   };
 };
 
