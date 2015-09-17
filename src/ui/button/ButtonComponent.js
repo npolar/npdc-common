60eca684a4897f6afd720a90bd11d0257ab96d45
Anders Bälter <anders.balter@npolar.no>
2015-09-17 11:09:42 +0200

    Moved buttons around and made them bigger

--- src/ui/button/ButtonComponent.js
+++ src/ui/button/ButtonComponent.js
@@ -1,8 +1,23 @@
 'use strict';
 
 // @ngInject
-var DeleteButtonComponent = function ($scope, NpolarApiSecurity) {  
+var DeleteButtonComponent = function ($scope, $mdDialog, NpolarApiSecurity) {
   $scope.security = NpolarApiSecurity;
+
+  $scope.confirmDelete = function (ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    var confirm = $mdDialog.confirm()
+          .title('Confirm')
+          .content('Are you sure you want to delete?')
+          .targetEvent(ev)
+          .ok('Delete')
+          .cancel('Cancle');
+    $mdDialog.show(confirm).then(function() {
+      $scope.delete();
+    }, function() {
+      // noop
+    });
+  };
 };
 
 module.exports = DeleteButtonComponent;
--- src/ui/formula/formula.js
+++ src/ui/formula/formula.js
@@ -5,7 +5,10 @@
  */
 var formula = function () {
   return {
-    templateUrl: 'npdc-common/src/ui/formula/edit.html'
+    templateUrl: 'npdc-common/src/ui/formula/edit.html',
+    link (scope) {
+      console.log('formula', scope.formula);
+    }
   };
 };
 
