ab47b82d4c80b86a11b8f3d842521c2fcc9155b7
Anders Bälter <anders.balter@npolar.no>
2015-10-01 16:05:28 +0200

    Fixed error reporting, added coverage

--- tasks/test.js
+++ tasks/test.js
@@ -3,14 +3,34 @@ var task = function(gulp, config) {
 
   var mocha = require('gulp-mocha');
   var notify = require('gulp-notify');
+  var istanbul = require('gulp-istanbul');
 
-  gulp.task('test', function () {
-    return gulp.src(config.tests, {read: false})
+  gulp.task('pre-test', function() {
+    return gulp.src(config.src.jsNoTests)
+      // Covering files
+      .pipe(istanbul({includeUntested: true}))
+      // Force `require` to return covered files
+      .pipe(istanbul.hookRequire());
+  });
+
+  gulp.task('test', ['pre-test'], function(done) {
+    return gulp.src(config.tests, {
+        read: false
+      })
       // gulp-mocha needs filepaths so you can't have any plugins before it
       .pipe(mocha({
         reporter: 'dot'
-        }))
-      .on('error', notify.onError({message: '<%= error.message %>', title: 'Gulp mocha'}));
+      }))
+      .on('error', function (error) {
+        notify({
+          message: '<%= error.message %>',
+          title: 'Gulp mocha'
+        }).write(error);
+        this.emit('end');
+      })
+      .pipe(istanbul.writeReports({
+        reporters: ['text-summary', 'lcov']
+      }));
   });
 };
 
