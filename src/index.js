895d6d6faf802a7dbca279af2b67ec84a11f0783
Anders Bälter <anders.balter@npolar.no>
2015-10-19 11:42:01 +0200

    Theming under way

--- src/index.js
+++ src/index.js
@@ -10,11 +10,17 @@ require('jusas-angularjs-slider');
 var ui = angular.module('npdcUi', ['npolarUi', 'npolarApi', 'ngMaterial', 'ngAnimate', 'rzModule']);
 
 ui.config(function($mdThemingProvider) {
-  var npdcPrimary = $mdThemingProvider.extendPalette('blue', {
-    '500': '1570b1'
+  var npdcPrimary = $mdThemingProvider.extendPalette('light-blue', {
+    '500': '0c3642',
+    '300': '0f3f4c',
+    '800': '427296',
+    'A100': 'ffffff',
+    'contrastDefaultColor': 'light',
+    'contrastDarkColors': ['300', '500', '800', 'A100']
   });
+  console.log(npdcPrimary);
   var npdcAccent = $mdThemingProvider.extendPalette('teal', {
-    'A200': '#C8FDEA'
+    'A200': 'C8FDEA'
   });
   $mdThemingProvider.definePalette('npdcPrimary', npdcPrimary);
   $mdThemingProvider.definePalette('npdcAccent', npdcAccent);
