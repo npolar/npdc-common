14ba4fa60145c4560b48378cff7ec30058ec5a0a
cnrdh <cnrdh@users.noreply.github.com>
2015-10-27 09:39:48 +0100

    Injected security

--- src/components/toolbar/toolbarDirective.js
+++ src/components/toolbar/toolbarDirective.js
@@ -1,13 +1,16 @@
 "use strict";
 
 // @ngInject
-let toolbar = function ($mdSidenav) {
+let toolbar = function ($mdSidenav, NpolarApiSecurity) {
     return {
       restrict: 'E',
       scope: {
         options: '='
       },
       template: require('./toolbar.html'),
+      link: function(scope) {
+        scope.security = NpolarApiSecurity;
+      }
     };
   };
 
