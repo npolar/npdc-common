d8fb12ac4ae3e1add78a1f7fb8dfdd4a155209ec
RDuX <ruben.dens@gmail.com>
2015-10-29 11:27:24 +0100

    made search result fields injectable

--- src/components/search/results/SearchResultsController.js
+++ src/components/search/results/SearchResultsController.js
@@ -2,6 +2,7 @@
 
 // @ngInject
 var SearchResultsController = function($scope, $location, NpolarApiSecurity, npdcAppConfig) {
+  $scope.options = npdcAppConfig.search.results;
 
   $scope.security = NpolarApiSecurity;
   $scope.q = $location.search().q;
@@ -24,12 +25,65 @@ var SearchResultsController = function($scope, $location, NpolarApiSecurity, npd
     }
   };
 
+  $scope.avatar = (entry) => {
+    if ($scope.options.avatar) {
+      return valueFromPath(entry, $scope.options.avatar);
+    }
+
+    return entry.id.substr(0,3);
+  };
+
   $scope.title = (entry) => {
+    if ($scope.options.title) {
+      return valueFromPath(entry, $scope.options.title);
+    }
+
     if (entry.titles) {
       return entry.titles[0].title;
     }
+
     return entry.title || entry.name || entry.code || entry.id;
   };
+
+  $scope.subTitle = (entry) => {
+    if ($scope.options.subtitle) {
+      return valueFromPath(entry, $scope.options.subtitle);
+    }
+    return entry.created_by;
+  };
+
+  $scope.detail = (entry) => {
+    if ($scope.options.detail) {
+      return valueFromPath(entry, $scope.options.detail);
+    }
+
+    return entry.updated.split("T")[0];
+  };
+
+
+  function valueFromPath(object, path, separator) {
+    if("object" === typeof object && "string" === typeof path) {
+      var node = object;
+
+      if("string" !== typeof separator || !separator.length) {
+        separator = "/";
+      }
+
+      for(var segment; path && (segment = path.split(separator)[0]); path = path.slice(segment.length + separator.length)) {
+        if("object" === typeof node) {
+          node = node[segment];
+        }
+      }
+
+      if (node instanceof Array) {
+        node = node.join(', ');
+      }
+
+      return node;
+    }
+
+    return undefined;
+  }
 };
 
 module.exports = SearchResultsController;
