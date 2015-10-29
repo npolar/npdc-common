d085d06c82ed2169189d0a214b446d5ac4734ae9
Remi Aleksander Solås <remi@npolar.no>
2015-10-29 11:50:53 +0100

    Added options attrib to npdc:toolbar

--- src/layouts/start2/startDemo.js
+++ src/layouts/start2/startDemo.js
@@ -3,4 +3,7 @@
 require('../../');
 let angular = require('angular');
 
-angular.module('start', ['npdcUi']);
+angular.module('start', ['npdcUi'])
+.controller('Start2DemoController', function(npdcAppConfig, $scope) {
+	$scope.options = npdcAppConfig;
+});
