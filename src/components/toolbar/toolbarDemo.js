ea9363fdc4c019ee8b0ed8d6e8d51954f3695b0b
Remi Aleksander Solås <remi@npolar.no>
2015-10-30 10:45:58 +0100

    Removed out-dated options block from toolbar demo script

--- src/components/toolbar/toolbarDemo.js
+++ src/components/toolbar/toolbarDemo.js
@@ -5,18 +5,4 @@ let angular = require('angular');
 
 angular.module('toolbar', ['npdcUi']).controller('ToolbarCtrl', function($scope, npdcAppConfig) {
   $scope.options = npdcAppConfig;
-  $scope.options.search = {
-    facets: require('../search/faceting/demo/facets.json').facets,
-    filterUi: {
-      'draft': {
-        type: 'checkbox'
-      },
-      'year-released': {
-        type: 'range'
-      },
-      'coverage.south': {
-        type: 'range'
-      }
-    }
-  };
 });
