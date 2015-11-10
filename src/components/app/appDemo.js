1af4e4fda0f41034ab5153fd31e8e5d6fca6e956
Anders Bälter <anders.balter@npolar.no>
2015-11-10 15:03:13 +0100

    Toast colors

--- src/components/app/appDemo.js
+++ src/components/app/appDemo.js
@@ -5,9 +5,17 @@ let angular = require('angular');
 
 let appDemo = angular.module('appDemo', ['npdcUi', 'templates']);
 
-appDemo.controller('demoCtrl', ($scope, NpolarApiSecurity) => {
+appDemo.controller('demoCtrl', ($scope, NpolarApiSecurity, NpolarApiMessage) => {
   NpolarApiSecurity.isAuthorized = () => true;
   $scope.resource = {
     path: '/demo'
+  };
+
+  $scope.error = function () {
+    NpolarApiMessage.emit('npolar-error', 'error');
+  };
+
+  $scope.info = function () {
+    NpolarApiMessage.emit('npolar-info', 'info');
   };
 });
