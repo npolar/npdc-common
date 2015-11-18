1836576ba5045392f9af0473f48cbfac27ccd8ef
Anders Bälter <anders.balter@npolar.no>
2015-11-18 16:40:41 +0100

    Support first_name, last_name and name

--- src/components/document/authors/authorsDirective.js
+++ src/components/document/authors/authorsDirective.js
@@ -10,7 +10,11 @@ var authorsDirective = function() {
     //@ngInject
     controller: function($scope) {
       $scope.fullnames = function (authors) {
-        return authors.reduce((m, a, i) => m + a.first_name + ' ' + a.last_name + (i < authors.length-1 ? ', ':''), '');
+        return authors.reduce((m, a, i) => {
+          return m +
+            (a.first_name ? a.first_name + ' ' + a.last_name : a.name) +
+            (i < authors.length-1 ? ', ':'');
+        }, '');
       };
     }
   };
