c196c1fce877f10b3c81c449b25d265493263a4c
Anders Bälter <anders.balter@npolar.no>
2015-12-14 16:34:51 +0100

    Relevant documents is now a array

--- src/components/document/links/linksDirective.js
+++ src/components/document/links/linksDirective.js
@@ -7,7 +7,14 @@ var linkDirective = function() {
       links: '=',
       related: '='
     },
-    template: require('./linkstemplate.html')
+    template: require('./linkstemplate.html'),
+    //@ngInject
+    controller($scope) {
+      $scope.title = function (collection) {
+        var str = collection[0].collection;
+        return str.charAt(0).toUpperCase() + str.slice(1);
+      };
+    }
   };
 };
 
