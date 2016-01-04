29fcc6d02ecfc1cab70538cb0f4a7c13625a8227
Anders Bälter <anders.balter@npolar.no>
2016-01-04 13:40:59 +0100

    More tests in formula demo

--- src/components/formula/formulaDemo.js
+++ src/components/formula/formulaDemo.js
@@ -6,7 +6,7 @@ let angular = require('angular');
 
 angular
   .module('formulaDemo', ['npdcCommon', 'formula'])
-  .controller('FormulaCtrl', ($mdDialog, $scope, $compile, formulaAutoCompleteService) => {
+  .controller('FormulaCtrl', ($mdDialog, $scope, $compile, $timeout, formulaAutoCompleteService) => {
     $scope.formulaData = {
       schema: "./demo/schema.json",
       form: "./demo/form.json",
@@ -21,6 +21,42 @@ angular
         }
       ]
     };
+
+    let updateModel = function() {
+      $scope.formulaData.model = {
+        _id: 'foobarID',
+        string: 'timeoutfoobar',
+        boolean: true,
+        array_object: [{
+          string_default: 'foo',
+          number: 1
+        }, {
+          string_default: 'bar',
+          number: 2
+        }],
+        array_string_enum: ['foo', 'qux'],
+        array_string: ['foobar', 'bazquz'],
+        ref_object: {
+          name: 'test',
+          email: 'foo',
+          dn: 'no'
+        },
+        nested_array: [
+          [1, 2],
+          [1, 2]
+        ],
+        array_hierarchy: [{
+          sub_array_one: [{
+            sub_sub_array_one: [{
+              obj_1_1_1: 'foo',
+              obj_1_1_2: 4
+            }]
+          }]
+        }]
+      };
+      console.log("timeout", $scope.formulaData.model.string);
+    };
+    $timeout(updateModel, 1000);
 
     let fn = function (q) {
       return ["Dalene", "Allan", "Lecia", "Leta", "Matthew", "Marlen", "Collette", "Alfredo", "Francina", "Dorene", "Ali", "Anette", "Courtney", "Arlena", "Spring", "Suzanna", "Roseanne", "Evita", "Gaynell", "Ellena", "Lucinda", "Delisa", "Lamont", "Eloy", "Luanna", "Cyndi", "Lynn", "Clare", "Stacey", "Tameka", "Cheryll", "Jong", "Hoyt", "Marhta", "Roselia", "Gala", "Chun", "Weston", "Zola", "Luana", "Arnette", "Delorse", "Libbie", "Nenita", "Lorina", "Carolyn", "Burma", "Russell", "Beatris", "Macie"];
