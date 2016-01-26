1ba322e908333ceab8d793995df8ac9e1f04e264
Anders Bälter <anders.balter@npolar.no>
2016-01-26 11:30:36 +0100

    Remi fixed it! Passing alot of tests!

--- src/components/formula/custom-templates/tabdata/csvService.js
+++ src/components/formula/custom-templates/tabdata/csvService.js
@@ -5,21 +5,81 @@
  */
 let csvService = function() {
 
-  let csvToJSON = function(csv, headers, options = { delimiter: "," }) {
-    let valuesRegExp = new RegExp('\s?(?:(".*?")(?:'+options.delimiter+'|\n|$)|([^'+options.delimiter+'"]+))','g');
-    return csv.split("\n").map(line => {
-      let match;
-      let item = {};
-      let i = 0;
-      while ((match = valuesRegExp.exec(line))) {
-        item[headers[i]] = match[0];
-        i++;
+  const DEFAULT_DELIMITER = ",";
+
+  let isBoolean = function (value) {
+    return ["true", "false"].indexOf(value.toLowerCase()) !== -1;
+  };
+
+  let parseValue = function(value) {
+    let num, parsed;
+    if (value === undefined) {
+      return undefined;
+    } else if (value.toLowerCase() === "nan") {
+      parsed = NaN;
+    } else if(!isNaN(num = Number(value))) {
+      parsed = num;
+    } else if (isBoolean(value)) {
+      parsed = JSON.parse(value.toLowerCase());
+    } else {
+      parsed = value;
+    }
+    return parsed;
+  };
+
+  let csvLineToArray = function(str, delimiter = DEFAULT_DELIMITER) {
+
+    let quotedValues = [],
+      values, match;
+    let expr = RegExp("([\"']{1}).*?(?=\\1\\s*(?:" + delimiter + "|$))", "g");
+
+    (str.match(expr) || [])
+    .forEach(function(quoted, index) {
+      quotedValues.push(quoted.replace(/["']/, ""));
+      str = str.replace(quoted, "{{%" + index + "%}}");
+    });
+
+    ((values = str.split(delimiter) || []))
+    .forEach(function(value, index, array) {
+      if ((match = value.match(/{{%(\d+)%}}/))) {
+        value = quotedValues[Number(match[1])];
       }
-      return item;
+      value = value.trim();
+      value = value === '' ? undefined : value;
+      array[index] = value;
+    });
+
+    return values;
+  };
+
+  let csvToJSON = function(csv, headers, options = { delimiter: DEFAULT_DELIMITER }) {
+    let lines = csv.split(/\r?\n\r?/);
+    if (!headers) {
+      headers = csvLineToArray(lines.shift());
+    }
+
+    return lines.map(line => {
+      let record =  csvLineToArray(line, options.delimiter).reduce((memo, item, index) => {
+        if (index < headers.length) {
+          memo[headers[index]] = parseValue(item);
+        }
+        return memo;
+      }, {});
+
+      // fill missing values with undefined
+      for (let i = headers.length - 1; i >= 0; i--) {
+        if (record.hasOwnProperty([headers[i]])) {
+          break;
+        } else {
+          record[headers[i]] = undefined;
+        }
+      }
+
+      return record;
     });
   };
 
-  let jsonToCSV = function(json, options = { delimiter: "," }) {
+  let jsonToCSV = function(json, options = { delimiter: DEFAULT_DELIMITER }) {
     let values = (json instanceof String) ? JSON.parse(json) : json;
     let csv = '';
     values.forEach((item, rowIndex) => {
--- src/components/formula/custom-templates/tabdata/csvServiceSpec.js
+++ src/components/formula/custom-templates/tabdata/csvServiceSpec.js
@@ -2,12 +2,13 @@
 require('should');
 
 var csvService = require('./csvService')();
+var spectrum = require('csv-spectrum');
 
 describe('csvService', function() {
   describe('#csvToJSON', function() {
     it('should parse', function() {
       var data =
-`some,values
+` some,values
 more,data`;
       var expected = [{h1: "some", h2: "values"},{h1: 'more', h2: "data"}];
 
@@ -16,9 +17,74 @@ more,data`;
 
     it('should not split on comma in quoted string', function() {
       var data = "\"some,values\"";
-      var expected = [{h1: "\"some,values\""}];
+      var expected = [{h1: "some,values", h2: undefined}];
 
       csvService.csvToJSON(data, ['h1', 'h2']).should.eql(expected);
+    });
+
+    it('should parse number as numbers', function() {
+      var data = "2,-1,0,0.0,10e-2,NaN,Infinity,-Infinity";
+      var expected = [{
+        uint: 2,
+        sint: -1,
+        zero: 0,
+        float: 0.0,
+        scientific: 10e-2,
+        nan: NaN,
+        inf: Infinity,
+        neginf: -Infinity
+      }];
+
+      csvService.csvToJSON(data, ['uint', 'sint', 'zero', 'float', 'scientific', 'nan', 'inf', 'neginf']).should.eql(expected);
+    });
+
+    it('should parse booleans as boolean', function() {
+      var data = "true,false,TRUE,FALSE";
+      var expected = [{
+        true: true,
+        false: false,
+        TRUE: true,
+        FALSE: false
+      }];
+
+      csvService.csvToJSON(data, ['true', 'false', 'TRUE', 'FALSE']).should.eql(expected);
+    });
+
+    it('should parse empty value as undefined', function() {
+      var data = ",";
+      var expected = [{
+        udef1: undefined,
+        udef2: undefined
+      }];
+
+      csvService.csvToJSON(data, ['udef1','udef2']).should.eql(expected);
+    });
+
+    it('should fill missing values with undefined', function() {
+      var data = "a";
+      var expected = [{
+        h1: "a",
+        h2: undefined
+      }];
+
+      csvService.csvToJSON(data, ['h1','h2']).should.eql(expected);
+    });
+
+    it('should skip values out of header bounds', function() {
+      var data = "a,b,c";
+      var expected = [{
+        h1: "a"
+      }];
+
+      csvService.csvToJSON(data, ['h1']).should.eql(expected);
+    });
+
+    it('should pass spectrum test suite', function () {
+      spectrum((err, data) => {
+        data.forEach(td => {
+          csvService.csvToJSON(td.csv.toString()).should.eql(JSON.parse(td.json.toString()));
+        });
+      });
     });
   });
 
