1aef9242d08b37934d37e5970b790d3b739fd488
Anders Bälter <anders.balter@npolar.no>
2016-01-26 16:58:37 +0100

    tabdata v2

--- src/components/formula/custom-templates/tabdata/csvServiceSpec.js
+++ src/components/formula/custom-templates/tabdata/csvServiceSpec.js
@@ -12,14 +12,14 @@ describe('csvService', function() {
 more,data`;
       var expected = [{h1: "some", h2: "values"},{h1: 'more', h2: "data"}];
 
-      csvService.csvToJSON(data, ['h1', 'h2']).should.eql(expected);
+      csvService.csvToJSON(data, {headers: ['h1', 'h2']}).should.eql(expected);
     });
 
     it('should not split on comma in quoted string', function() {
       var data = "\"some,values\"";
       var expected = [{h1: "some,values", h2: undefined}];
 
-      csvService.csvToJSON(data, ['h1', 'h2']).should.eql(expected);
+      csvService.csvToJSON(data, {headers: ['h1', 'h2']}).should.eql(expected);
     });
 
     it('should parse number as numbers', function() {
@@ -35,19 +35,19 @@ more,data`;
         neginf: -Infinity
       }];
 
-      csvService.csvToJSON(data, ['uint', 'sint', 'zero', 'float', 'scientific', 'nan', 'inf', 'neginf']).should.eql(expected);
+      csvService.csvToJSON(data, {headers: ['uint', 'sint', 'zero', 'float', 'scientific', 'nan', 'inf', 'neginf']}).should.eql(expected);
     });
 
     it('should parse booleans as boolean', function() {
       var data = "true,false,TRUE,FALSE";
       var expected = [{
-        true: true,
-        false: false,
-        TRUE: true,
-        FALSE: false
+        true1: true,
+        false1: false,
+        TRUE2: true,
+        FALSE2: false
       }];
 
-      csvService.csvToJSON(data, ['true', 'false', 'TRUE', 'FALSE']).should.eql(expected);
+      csvService.csvToJSON(data, {headers: ['true1', 'false1', 'TRUE2', 'FALSE2']}).should.eql(expected);
     });
 
     it('should parse empty value as undefined', function() {
@@ -57,7 +57,7 @@ more,data`;
         udef2: undefined
       }];
 
-      csvService.csvToJSON(data, ['udef1','udef2']).should.eql(expected);
+      csvService.csvToJSON(data, {headers: ['udef1','udef2']}).should.eql(expected);
     });
 
     it('should fill missing values with undefined', function() {
@@ -67,7 +67,7 @@ more,data`;
         h2: undefined
       }];
 
-      csvService.csvToJSON(data, ['h1','h2']).should.eql(expected);
+      csvService.csvToJSON(data, {headers: ['h1','h2']}).should.eql(expected);
     });
 
     it('should skip values out of header bounds', function() {
@@ -76,7 +76,14 @@ more,data`;
         h1: "a"
       }];
 
-      csvService.csvToJSON(data, ['h1']).should.eql(expected);
+      csvService.csvToJSON(data, {headers: ['h1']}).should.eql(expected);
+    });
+
+    it('should skip empty lines', function() {
+      var data = "a\r\n\r\na";
+      var expected = Array(2).fill({'h1': 'a'});
+
+      csvService.csvToJSON(data, {headers: ['h1']}).should.eql(expected);
     });
 
     it('should pass spectrum test suite', function () {
@@ -93,7 +100,7 @@ more,data`;
       var data = [{h1: "some", h2: "values"},{h1: "more", h2: "data"}];
       var expected = "some,values\r\nmore,data";
 
-      csvService.jsonToCSV(data).should.eql(expected);
+      csvService.jsonToCSV(data, {skipHeaders: true, headers: ['h1','h2']}).should.eql(expected);
     });
   });
 });
--- src/components/formula/custom-templates/tabdata/tabdataDirective.js
+++ src/components/formula/custom-templates/tabdata/tabdataDirective.js
@@ -1,38 +1,90 @@
 'use strict';
 
-/**
- * @ngInject
- */
 let tabdata = function(npdcCSVService) {
+  'ngInject';
+
+  const DELIMITERS = {
+    '\t': 'TAB',
+    ',': 'COMMA (,)',
+    ';': 'SEMICOLON (;)',
+    ' ': 'SPACE ( )',
+    '|': 'PIPE (|)'
+  };
+
+  let applySchemaValueType = function (item, schema) {
+    Object.keys(item).forEach(prop => {
+      let type;
+      if (schema[prop] && (type = schema[prop].type)) {
+        if (type instanceof Array) {
+          type = type.find(t => t !== 'null') || 'string';
+        }
+
+        if (typeof item[prop] !== type) {
+          switch (type) {
+            case 'string':
+              item[prop] = JSON.stringify(item[prop]);
+              break;
+          }
+        }
+      }
+    });
+    return item;
+  };
 
   return {
     template: require('./tabdata.html'),
     //@ngInject
     controller($scope) {
-      $scope.csvData = npdcCSVService.jsonToCSV($scope.field.value);
-      let headers = $scope.field.fields[0].fields.map(field => field.id);
+      let headers = Object.keys($scope.field.schema.items.properties);
+      let delimiter = ';';
+      $scope.delimiter = DELIMITERS[delimiter];
+      $scope.csvData = npdcCSVService.jsonToCSV($scope.field.value, {headers, delimiter: delimiter});
+      let headerString = headers.toString();
+      $scope.csvHeader = headerString.replace(/,/g, delimiter);
       $scope.$watch('csvData', (n, o) => {
         if (n && n !== o) {
           let model = {};
-          model[$scope.field.id] = npdcCSVService.csvToJSON(n, headers);
-          $scope.field.valueFromModel(model);
+          let headerLine = n.split(/\r?\n\r?/)[0];
+          let detectedDelimiter = /([\t\s,;|])/.exec(headerLine)[1];
+          $scope.csvHeader = headerLine;
+          $scope.delimiter = DELIMITERS[detectedDelimiter];
+
+          if (!npdcCSVService.isMatchingHeaders(npdcCSVService.csvLineToArray(headerLine, detectedDelimiter), headers)) {
+            $scope.field.valid = false;
+            $scope.field.errors = [{message: "Invalid header line, should use names: " + headerString}];
+          } else {
+            $scope.field.valid = true;
+            $scope.field.errors = null;
+            model[$scope.field.id] = npdcCSVService.csvToJSON(n, {headers, delimiter}).map(item => {
+              applySchemaValueType(item, $scope.field.schema.items.properties);
+              Object.keys(item).forEach(prop => {
+                if (item[prop] === undefined) {
+                  delete item[prop];
+                }
+              });
+              return item;
+            });
+            $scope.field.valueFromModel(model);
+            $scope.csvData.replace(detectedDelimiter, delimiter, 'g');
+            $scope.$emit('revalidate');
+          }
         }
       });
     },
     link(scope, element, attrs) {
-      var move = false;
-      var lineNumbers = document.createElement('TEXTAREA');
-      var textarea = element.find('textarea')[0];
-
-      var string = '';
-      for (var no = 1; no < 300; no++) {
+      let move = false;
+      let lineNumbers = document.createElement('TEXTAREA');
+      let textarea = element.find('textarea')[0];
+      let noOfLines = scope.csvData.split('\r?\n\r?').length;
+      let string = '';
+      for (let no = 0; no < Math.max(noOfLines + 500); no++) {
         if (string.length > 0) {
           string += '&#10;';
         }
         string += no;
       }
-      lineNumbers.className = 'line-numbers md-input';
       lineNumbers.innerHTML = string;
+      lineNumbers.className = 'line-numbers md-input';
       textarea.parentNode.insertBefore(lineNumbers, textarea.nextSibling);
 
       function setLine() {
@@ -54,6 +106,25 @@ let tabdata = function(npdcCSVService) {
       textarea.onmousemove = function() {
         if (move) {
           setLine();
+        }
+      };
+      textarea.onkeydown = function(e) {
+        if(e.keyCode === 9) { // tab was pressed
+          // get caret position/selection
+          let start = this.selectionStart;
+          let end = this.selectionEnd;
+
+          let target = e.target;
+          let value = target.value;
+
+          // set textarea value to: text before caret + tab + text after caret
+          target.value = value.substring(0, start) + "\t" + value.substring(end);
+
+          // put caret at right position again (add one for the tab)
+          this.selectionStart = this.selectionEnd = start + 1;
+
+          // prevent the focus lose
+          e.preventDefault();
         }
       };
     }
