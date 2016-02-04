8da7b47a9fa5ada50c3ee1d2f7974885dc890723
Anders Bälter <anders.balter@npolar.no>
2016-02-04 16:01:44 +0100

    i18n work

--- src/components/app/AppController.js
+++ src/components/app/AppController.js
@@ -1,12 +1,18 @@
 "use strict";
 
-let AppWrapperCtrl = function($location, $scope, npolarApiConfig, npdcAppConfig, NpolarApiSecurity) {
+let AppWrapperCtrl = function($location, $scope, npolarApiConfig, npdcAppConfig, NpolarApiSecurity, NpolarTranslate) {
   'ngInject';
+
+  const APP_TITLE_CODE = 'npdc.app.Title';
 
   $scope.options = npdcAppConfig;
 
   $scope.security = NpolarApiSecurity;
   $scope.q = $location.search().q;
+  $scope.cardTitle = function () {
+    var i18nTitle = NpolarTranslate.translate(npdcAppConfig.cardTitle);
+    return i18nTitle === APP_TITLE_CODE ? npdcAppConfig.toolbarTitle : i18nTitle;
+  };
 
   if ("production" === npolarApiConfig.environment) {
     if ("data.npolar.no" !== window.location.hostname) {
