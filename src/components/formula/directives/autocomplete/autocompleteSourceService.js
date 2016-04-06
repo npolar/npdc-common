10e3159eb8683879470e7f840dec01e78c20f193
Anders Bälter <anders.balter@npolar.no>
2016-04-06 12:00:37 +0200

    Better autocomplete facet matching

--- src/components/formula/directives/autocomplete/autocompleteSourceService.js
+++ src/components/formula/directives/autocomplete/autocompleteSourceService.js
@@ -23,24 +23,13 @@ let autocompleteSourceService = function($q, formulaFieldConfig) {
   let autocompleteFacets = function (autocompleteFacets, resource, formula) {
     let facets = autocompleteFacets.join(',');
 
-    $q.all([resource.facets({ facets, q: '' }).$promise, formula.getFields()]).then(responses => {
+    resource.facets({ facets, q: '' }, facets => {
       autocompleteFacets.forEach(facet => {
-        let item = responses[0].find(r => r.facet === facet);
+        let item = facets.find(r => r.facet === facet);
         let nodes = item.facet.split('.');
-        let fields = responses[1].filter(field => {
-          let parts = nodes.slice();
 
-          return field.id === parts.pop() &&
-            field.parents.map(p => p.id).filter(p => !/_item/.test(p))
-            .every(p => p === parts.pop());
-        });
-        if (!fields.length) {
-          console.error("No field matching facet " + facet);
-          return;
-        }
-        console.log('Found fields', JSON.stringify(fields.map(f => f.path)), 'for', facet);
         let source = function (q) {
-          q = (q || '').toLocaleLowerCase();
+          q = String(q || '').toLocaleLowerCase();
           return item.terms.map(t => String(t.term))
             .filter(item => item.toLocaleLowerCase().indexOf(q) !== -1)
             .sort((a, b) => {
@@ -55,21 +44,14 @@ let autocompleteSourceService = function($q, formulaFieldConfig) {
             });
         };
 
-        fields.forEach(field => {
-          let fNodes = nodes.slice();
-          if (field.typeOf('array')) {
-            fNodes.push(field.id + '_item');
-          }
-          let match = function(field) {
-            let fieldNodes = field.path.replace(/^#\//, '').replace(/\/\d$/, '/'+field.id).replace(/\/\d+\//g, '').split('/');
-            console.log('adding matching fn', fNodes, fieldNodes);
-            return angular.equals(fNodes, fieldNodes);
-          };
-          autocomplete({
-            match,
-            querySource: source
-          }, formula);
-        });
+        let match = function(field) {
+          let fieldNodes = field.path.replace(/^#\//, '').replace(/\/\d+/g, '').split('/');
+          return !field.typeOf('array') && angular.equals(nodes, fieldNodes);
+        };
+        autocomplete({
+          match,
+          querySource: source
+        }, formula);
 
       });
     });
