3a957cbf9837a69cfb051dffc438bf2cb190a1fc
cnrdh <cnrdh@users.noreply.github.com>
2016-11-29 15:51:27 +0100

    Use explicit protocol as these are sometimes exposed to users

--- src/components/document/badges/badgesDirective.js
+++ src/components/document/badges/badgesDirective.js
@@ -14,13 +14,13 @@ module.exports = function() {
       "ngInject";
 
       var licenses = {
-        "cc-by-nc-nd":  { img: "licenses/cc/by-nc-nd",  href: "//creativecommons.org/licenses/by-nc-nd/4.0/" },
-        "cc-by-nc-sa":  { img: "licenses/cc/by-nc-sa",  href: "//creativecommons.org/licenses/by-nc-sa/4.0/" },
-        "cc-by-nc":     { img: "licenses/cc/by-nc",     href: "//creativecommons.org/licenses/by-nc/4.0/" },
-        "cc-by-nd":     { img: "licenses/cc/by-nd",     href: "//creativecommons.org/licenses/by-nd/4.0/" },
-        "cc-by-sa":     { img: "licenses/cc/by-sa",     href: "//creativecommons.org/licenses/by-sa/4.0/" },
-        "cc-by":        { img: "licenses/cc/by",        href: "//creativecommons.org/licenses/by/4.0/"   },
-        "cc-zero":      { img: "licenses/cc/zero",      href: "//creativecommons.org/about/cc0/" }
+        "cc-by-nc-nd":  { img: "licenses/cc/by-nc-nd",  href: "https://creativecommons.org/licenses/by-nc-nd/4.0/" },
+        "cc-by-nc-sa":  { img: "licenses/cc/by-nc-sa",  href: "https://creativecommons.org/licenses/by-nc-sa/4.0/" },
+        "cc-by-nc":     { img: "licenses/cc/by-nc",     href: "https://creativecommons.org/licenses/by-nc/4.0/" },
+        "cc-by-nd":     { img: "licenses/cc/by-nd",     href: "https://creativecommons.org/licenses/by-nd/4.0/" },
+        "cc-by-sa":     { img: "licenses/cc/by-sa",     href: "https://creativecommons.org/licenses/by-sa/4.0/" },
+        "cc-by":        { img: "licenses/cc/by",        href: "https://creativecommons.org/licenses/by/4.0/"   },
+        "cc-zero":      { img: "licenses/cc/zero",      href: "https://creativecommons.org/publicdomain/zero/1.0/" }
       };
 
       Object.defineProperties($scope, {
