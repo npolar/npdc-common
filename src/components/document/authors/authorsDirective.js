7b08f760adfacbd4320452f464e413153a20f792
cnrdh <cnrdh@users.noreply.github.com>
2016-11-29 11:14:57 +0100

    Removing fat NP people

--- src/components/document/authors/authorsDirective.js
+++ src/components/document/authors/authorsDirective.js
@@ -1,24 +1,30 @@
 'use strict';
-
-var authorsDirective = function() {
+let authorsDirective = function() {
   'ngInject';
 
   return {
     scope: {
-      authors: '='
+      authors: '=',
+      config: '=?'
     },
     template: require('./authorstemplate.html'),
     controller: function($scope) {
       'ngInject';
       
-      $scope.authors = $scope.authors.map(a => {
-        if (!a.name && a.last_name) {
+      let self = this;
+      
+      $scope.authors = ($scope.authors||[]).map(a => {
+      
+        if (a && a.last_name) {  
           a.name = `${a.first_name||''} ${a.last_name}`.trim();
         }
-        //delete a.first_name;
-        //delete a.last_name;
         return a;
       });
+      
+      $scope.isNpolar = function(o) {
+        return ((/npolar\.no|NPI|Norsk Polarinstitutt|Norwegian Polar Institute/i).test(o));
+      };
+      
     }
   };
 };
