764b20ca5acf57fb125f3edd4460cd9584a0944d
cnrdh <cnrdh@users.noreply.github.com>
2016-12-13 11:11:44 +0100

    Refined authors and citation components

--- src/components/document/links/linksDirective.js
+++ src/components/document/links/linksDirective.js
@@ -11,11 +11,6 @@ var linkDirective = function() {
     template: require('./linkstemplate.html'),
     controller($scope) {
       'ngInject';
-      
-      $scope.title = function (collection) {
-        var str = collection[0].collection;
-        return str.charAt(0).toUpperCase() + str.slice(1);
-      };
 
       $scope.isEmpty = function(related) {
         return !!related && !related.some(collection => collection.length > 0);
