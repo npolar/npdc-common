f5ee126056927f6e75ad96adb32f72a6df8b38f3
cnrdh <cnrdh@users.noreply.github.com>
2017-02-01 21:32:21 +0100

    Added duplicate; fixed title link when editing

--- src/components/formula/formulaDirective.js
+++ src/components/formula/formulaDirective.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var formula = function($mdDialog, $location, $routeParams, npdcAppConfig, npolarDocumentUtil, NpolarTranslate) {
+var formula = function($mdDialog, $location, $routeParams, $filter,
+  npdcAppConfig, NpolarTranslate) {
   'ngInject';
 
   return {
@@ -14,21 +15,47 @@ var formula = function($mdDialog, $location, $routeParams, npdcAppConfig, npolar
           items: [],
           alwaysShow: false
         };
+
+
         if ($scope.security.isAuthorized('delete', $scope.resource.path) && $scope.document._rev) {
+          let name = 'Delete';
           $scope.bottomSheetOptions.items.push({
-            name: NpolarTranslate.translate('Delete'),
+            name: NpolarTranslate.translate(name),
             icon: 'delete',
             classes: 'md-warn',
             action: function(ev) {
               // Appending dialog to document.body to cover sidenav in docs app
               var confirm = $mdDialog.confirm()
-                .title(NpolarTranslate.translate('Delete'))
+                .title(NpolarTranslate.translate('Confirm delete'))
                 .textContent(NpolarTranslate.translate('delete.confirm'))
                 .targetEvent(ev)
-                .ok(NpolarTranslate.translate('Delete'))
+                .ok(NpolarTranslate.translate(name))
                 .cancel(NpolarTranslate.translate('Cancel'));
               $mdDialog.show(confirm).then(function() {
                 $scope.delete();
+              }, function() {
+                // noop
+              });
+            }
+          });
+        }
+
+
+        if ($scope.security.isAuthorized('create', $scope.resource.path) && $scope.document._rev) {
+          let duplicate = 'Duplicate';
+          $scope.bottomSheetOptions.items.push({
+            name: NpolarTranslate.translate(duplicate),
+            icon: 'control_point_duplicate',
+            classes: 'md-primary',
+            action: function(ev) {
+              var confirm = $mdDialog.confirm()
+                .title(NpolarTranslate.translate('Confirm duplication'))
+                .textContent(NpolarTranslate.translate('Confirm that you want to create a copy of the current document'))
+                .targetEvent(ev)
+                .ok(NpolarTranslate.translate(duplicate))
+                .cancel(NpolarTranslate.translate('Cancel'));
+              $mdDialog.show(confirm).then(function() {
+                $scope.duplicate($scope.document, true);
               }, function() {
                 // noop
               });
@@ -39,6 +66,9 @@ var formula = function($mdDialog, $location, $routeParams, npdcAppConfig, npolar
 
       $scope.$on('npolar-lang', (e, lang) => {
         $scope.formula.i18n.set(lang.lang);
+        if ($scope.document && $scope.document._rev && $scope.document.title) {
+          $scope.title = $filter('i18n')($scope.document.title);
+        }
       });
 
       // @FIXME Sometimes goes bananas and spams dialogs...
@@ -60,13 +90,19 @@ var formula = function($mdDialog, $location, $routeParams, npdcAppConfig, npolar
         return $scope.resource ? $scope.resource.href(id) : id;
       };
 
-      $scope.$watch('document', (newVal, oldVal) => {
-        if (newVal) {
-          $scope.title = newVal._rev ? npolarDocumentUtil.title(newVal) :
-            NpolarTranslate.translate('document.new');
+      $scope.$watch('document', (d, was) => {
+        if (d) {
+          if (d._rev) {
+            if (d.title) {
+              $scope.title = $filter('i18n')(d.title);
+            }
+          } else {
+            $scope.title = NpolarTranslate.translate('document.new');
+          }
           initBottomSheet();
         }
       });
+
     }
   };
 };
